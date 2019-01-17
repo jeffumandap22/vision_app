d723ecf29f1053aa290089fb90d17ad94205f6e4
Jeffrey Umandap <jeffreyumandap@Jeff-Umandap-MacBook-Pro.local>
2019-01-17 13:42:56 +0800

    first commit

---
+++ VisionApp/VisionApp/Controller/CameraVC.swift
@@ -0,0 +1,25 @@
+//
+//  CameraVC.swift
+//  VisionApp
+//
+//  Created by Jeffrey Umandap on 17/01/2019.
+//  Copyright © 2019 Jeffrey Umandap. All rights reserved.
+//
+
+import UIKit
+
+class CameraVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
